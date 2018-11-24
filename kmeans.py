4d49bc17e7c0edbea722cb1fe7a1a3aac119ee6f
RaiNIng <zhngyuhng@gmail.com>
2018-11-24 14:25:01 -0600

    acceptable results got, keep wokring

--- kmeans.py
+++ kmeans.py
@@ -13,24 +13,7 @@ def assign_label_cluster(distance, data_point, centroids):
 def compute_new_centroids(cluster_label, centroids):
     return np.array(cluster_label + centroids)/2
 
-def iterate_k_means(data_points, centroids, total_iteration):
-    label = []
-    cluster_label = []
-    total_points = len(data_points)
-    k = len(centroids)
-    
-    for iteration in range(0, total_iteration):
-        for index_point in range(0, total_points):
-            distance = {}
-            for index_centroid in range(0, k):
-                distance[index_centroid] = compute_euclidean_distance(data_points[index_point], centroids[index_centroid])
-            label = assign_label_cluster(distance, data_points[index_point], centroids)
-            centroids[label[0]] = compute_new_centroids(label[1], centroids[label[0]])
 
-            if iteration == (total_iteration - 1):
-                cluster_label.append(label)
-
-    return [cluster_label, centroids]
 
 def print_label_data(result):
     print("Result of k-Means Clustering: \n")
@@ -39,24 +22,55 @@ def print_label_data(result):
         print("cluster number: {} \n".format(data[0]))
     print("Last centroids position: \n {}".format(result[1]))
 
-def create_centroids():
+def create_centroids(lo, hi, k):
     centroids = []
-    centroids.append([5.0, 0.0])
-    centroids.append([45.0, 70.0])
-    centroids.append([50.0, 90.0])
+    for _ in xrange(k):
+        centroids.append([])
+        for i in xrange(len(lo)):
+            num = (hi[i] - lo[i]) * np.random.random() + lo[i]
+            centroids[-1].append(num)
     return np.array(centroids)
 
 if __name__ == "__main__":
     #filename = os.path.dirname(__file__) + "./data.csv"
     
     df = pd.read_csv("./iris.csv")
-    print(df)
-    plt.plot(df["sepal_length"], df["petal_length"], ".")
-    plt.show()
+    n, d = df.shape[0], df.shape[1] - 1
+
+
     #data_points = np.genfromtxt(filename, delimiter=",")
-    #centroids = create_centroids()
-    #total_iteration = 100
     
-    #[cluster_label, new_centroids] = iterate_k_means(data_points, centroids, total_iteration)
+    
+    niter = 30
+    nk = 3
+    
+    centroids = create_centroids([min(df.iloc[:,i]) for i in xrange(d)], [max(df.iloc[:,i]) for i in xrange(d)], nk)
+    
+    for iteration in xrange(niter):
+        nxt, nxt_count = [[0] * d for _ in xrange(nk)], [0] * nk
+        
+        for i in xrange(n):
+            min_dis, label = float("Inf"), -1
+            for j in range(nk):
+                dis = np.linalg.norm(centroids[j] - df.iloc[i].tolist()[:-1])
+                if dis < min_dis:
+                    min_dis, label = dis, j
+            
+            for k in xrange(d):
+                nxt[label][k] = (nxt[label][k] * nxt_count[label] + df.iloc[i][k]) / (nxt_count[label] + 1.0)
+            nxt_count[label] += 1
+
+        centroids = np.array(nxt)
+        print([list(x) for x in centroids])
+            #if iteration == (total_iteration - 1):
+            #    cluster_label.append(label)
+    
+    plt.plot(df.iloc[:, 1], df.iloc[:, 2], ".")
+    plt.plot(centroids[0][1], centroids[0][2], "*")
+    plt.plot(centroids[1][1], centroids[1][2], "*")
+    plt.plot(centroids[2][1], centroids[2][2], "*")
+    plt.show()
+    
     #print_label_data([cluster_label, new_centroids])
     print()
+    
